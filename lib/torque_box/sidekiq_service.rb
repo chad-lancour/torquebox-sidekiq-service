0ea4c43353d7eb330cf065fd3ad0d68aacea2d5d
Chad Lancour <chad.lancour@oracle.com>
2014-02-26 04:29:23 -0800

    debug output

--- lib/torque_box/sidekiq_service.rb
+++ lib/torque_box/sidekiq_service.rb
@@ -7,10 +7,14 @@ module TorqueBox
     CONFIG_OPTIONS_TO_STRIP = [:config_file, :daemon, :environment, :pidfile, :require, :tag]
 
     def initialize(opts = {})
+      @l = TorqueBox::Logger.new('sidekiq_init')
+      @l.info opts
       opts =  Hash[opts.map{|k,v| [:"#{k}",v] }]
+      @l.info opts
       setup_redis(opts.delete(:redis))
 
       @config = opts.reject { |k, _| CONFIG_OPTIONS_TO_STRIP.include?(k) }
+      @l.info @config
       @mutex = Mutex.new
     end
 
@@ -25,7 +29,7 @@ module TorqueBox
     def run
       Sidekiq.options.merge!(config)
       Sidekiq.options[:queues] << 'default' if Sidekiq.options[:queues].empty?
-
+      @l.info Sidekiq.options
       if Sidekiq.options.has_key?(:logfile)
         Sidekiq::Logging.initialize_logger(Sidekiq.options[:logfile])
       else
